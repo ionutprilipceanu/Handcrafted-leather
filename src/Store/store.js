225679338d871a8ff8ccfce966427d4cbf91df01
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-02 16:38:16 +0300

    up

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,6 +2,9 @@ import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 import { productsReducer } from '../reducers/productReducers';
 
+
+export const isBrowser = () => typeof window !== 'undefined'
+
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(combineReducers({
