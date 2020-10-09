6c1dff61533d47104ce0b6e8cb0920212be31898
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-09 17:04:03 +0300

    up

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ import favicon from '..//images/favicon.png'
             <meta property="og:description" content="My name is Marian Mihalache and I love to do stuff from handcrafted leather. Shop handcrafted wallets, bags, watch straps, accessories, gifts and more."/>
             <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
 
-            {/* <!-- Twitter S --> */}
+            {/* <!-- Twitter --> */}
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content="https://metatags.io/"/>
             <meta property="twitter:title" content="Marian Mihalache - Handcrafted leather | Iași  "/>
