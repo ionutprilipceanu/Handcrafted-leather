ce9989156127e499af039d55532bee4911a313e7
ionutprilipceanu <ionutprilipceanu@users.noreply.github.com>
2020-10-09 10:02:32 +0300

    up

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -2,11 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+
     <h1>Info</h1>
     <p>Thank you for your submission!</p>
     <Link to="/index.js">Go back to the home page</Link>
